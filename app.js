111a741035abfead41c0afeb5ddc8115a5fb7450
TheZincTZ <154614743+TheZincTZ@users.noreply.github.com>
2024-07-17 00:00:21 +0800

    completed CRUD

--- app.js
+++ app.js
@@ -42,6 +42,19 @@ app.use(express.urlencoded({
 
 // Define routes
 app.get('/',(req, res) => {
+    const sql = 'SELECT * FROM flower';
+    // fetch data from MySQL
+    connection.query(sql, (error, results) => {
+        if (error){
+            console.error('Database query error:', error.message);
+            return res.status(500).send('Error Retrieving students');
+        }
+        //render HTML page with data
+        res.render('index', {flower: results});
+    });
+});
+
+app.get('/cart',(req, res) => {
     const sql = 'SELECT * FROM cart';
     // fetch data from MySQL
     connection.query(sql, (error, results) => {
@@ -50,7 +63,7 @@ app.get('/',(req, res) => {
             return res.status(500).send('Error Retrieving students');
         }
         //render HTML page with data
-        res.render('index', {cart: results});
+        res.render('cart', {cart: results});
     });
 });
 
@@ -67,43 +80,33 @@ app.get('/subscription',(req, res) => {
     });
 });
 
-app.get('/customise',(req, res) => {
-    const sql = 'SELECT * FROM cart';
-    // fetch data from MySQL
-    connection.query(sql, (error, results) => {
-        if (error){
-            console.error('Database query error:', error.message);
-            return res.status(500).send('Error Retrieving students');
-        }
-        //render HTML page with data
-        res.render('customise', {cart: results});
-    });
-});
+// app.get('/customise',(req, res) => {
+//     const sql = 'SELECT * FROM flower';
+//     // fetch data from MySQL
+//     connection.query(sql, (error, results) => {
+//         if (error){
+//             console.error('Database query error:', error.message);
+//             return res.status(500).send('Error Retrieving students');
+//         }
+//         //render HTML page with data
+//         res.render('customise', {cart: results});
+//     });
+// });
 
-app.get('/cart', (req,res) => {
-    const customersql = 'SELECT * FROM subscription ';
-    connection.query(customersql, (error, results) => {
+app.get('/customise/:id', (req,res) => {
+    //Extract the student ID from the request parameters
+    const flower_id = req.params.id;
+    const sql = 'SELECT * FROM flower WHERE flower_id = ?';
+    // fetch data from MySQL based on the student ID
+    connection.query(sql, [flower_id], (error, results) => {
         if (error){
             console.error('Database query error :', error.message);
-            return res.status(500).send('Error Retrieving subscription data. subscription could be empty');
+            return res.status(500).send('Error Retrieving student by ID');
         }
         //check if any student with the gievn ID was found
         if (results.length > 0) {
-            const sql = 'SELECT * FROM cart INNER JOIN subscription ON subscription.customerid = cart.customerid WHERE cart.customerid = subscription.customerid';
-            connection.query(sql, (error, sqlresults) => {
-                if (error){
-                    console.error('Database query error :', error.message);
-                    return res.status(500).send('Error Retrieving cart data. Cart could be empty');
-                }
-                //check if any student with the gievn ID was found
-                if (sqlresults.length > 0) {
-                    //render HTML page with the student data
-                    res.render('cart', {cart : sqlresults});
-                } else {
-                    // if no student with the given ID was found, render a 404 page or handle it accordingly
-                    res.status(404).send('student not found');
-                }
-            });
+            //render HTML page with the student data
+            res.render('customise', {flower: results[0]});
         } else {
             // if no student with the given ID was found, render a 404 page or handle it accordingly
             res.status(404).send('student not found');
@@ -111,25 +114,60 @@ app.get('/cart', (req,res) => {
     });
 });
 
-app.post('/checkout', (req,res) => {
-    // extract student data from the request body
-    console.log(req.body)
-    const {address, postalcode, customerid,totalamount,cartorderid} = req.body;
+// app.post('/checkout', (req,res) => {
+//     // extract student data from the request body
+//     const {address, postalcode, customerid,totalamount,cartorderid} = req.body;
     
 
-    const sql  = "INSERT INTO checkout (orderdetail, orderdate, address, postalcode, totalamount, deliverydate, customerid) VALUES (?, CURRENT_DATE, ?, ?, ?, CURRENT_DATE + 7, ?)";
+//     const sql  = "INSERT INTO checkout (orderdetail, orderdate, address, postalcode, totalamount, deliverydate, customerid) VALUES (?, CURRENT_DATE, ?, ?, ?, CURRENT_DATE + 7, ?)";
+//     // insert the new student into the database
+//     connection.query(sql, [cartorderid,address,postalcode,totalamount,customerid], (error, results) =>{
+//         if (error) {
+//             //handle any error that occurs during the database operation
+//             console.error("Error checking out:", error);
+//             res.status(500).send('Error checking out');
+//         } else {
+//             //send a success response 
+//             res.redirect('checkout');
+//         }
+//     })
+// })
+
+app.post('/checkout', (req,res) => {
+    // extract student data from the request body
+    const {address, postalcode, customerid,totalamount,cartorderid} = req.body;
+    const sql  = "INSERT INTO checkout (cartorderId, orderdate, address, postalcode, totalamount, deliverydate, customerid) VALUES (?, CURRENT_DATE, ?, ?, ?, CURRENT_DATE + 7, ?)";
     // insert the new student into the database
     connection.query(sql, [cartorderid,address,postalcode,totalamount,customerid], (error, results) =>{
         if (error) {
             //handle any error that occurs during the database operation
-            console.error("Error checking out:", error);
-            res.status(500).send('Error checking out');
+            console.error("Error adding student:", error);
+            res.status(500).send('Error adding student');
         } else {
             //send a success response 
-            res.redirect('/checkout');
+            res.redirect('checkout');
         }
     })
 })
+
+app.get('/checkout',(req, res) => {
+    const sql = 'SELECT * FROM checkout';
+    // fetch data from MySQL
+    connection.query(sql, (error, results) => {
+        if (error){
+            console.error('Database query error :', error.message);
+            return res.status(500).send('Error Retrieving student by ID');
+        }
+        //check if any student with the gievn ID was found
+        if (results.length > 0) {
+            //render HTML page with the student data
+            res.render('checkout', {checkout: results[0]});
+        } else {
+            // if no student with the given ID was found, render a 404 page or handle it accordingly
+            res.status(404).send('student not found');
+        }
+    });
+});
 
 // app.get('/checkout', (req,res) => {
 //     const cartorderId = req.params.id;
@@ -164,7 +202,123 @@ app.get('/tracking',(req, res) => {
     });
 });
 
+app.get('/addflower',(req, res) => {
 
+        //render HTML page with data
+        res.render('addflower');
+});
+
+app.post('/addflower', (req,res) => {
+    // extract student data from the request body
+    const {title,img,descript,price} = req.body;
+    const sql  = "INSERT INTO flower (title, img, description, price) VALUES (?, ?, ?, ?)";
+    // insert the new student into the database
+    connection.query(sql, [title,img,descript,price], (error, results) =>{
+        if (error) {
+            //handle any error that occurs during the database operation
+            console.error("Error adding student:", error);
+            res.status(500).send('Error adding student');
+        } else {
+            //send a success response 
+            res.redirect('/addflower');
+        }
+    })
+})
+
+app.post('/payment', (req,res) => {
+    // extract student data from the request body
+    const {price, address, postal, date} = req.body;
+    const sql  = "DELETE FROM checkout WHERE orderdate = ? AND address = ? AND postalcode = ? AND totalamount = ?";
+    // insert the new student into the database
+    connection.query(sql, [date,address,postal,price], (error, results) =>{
+        if (error) {
+            //handle any error that occurs during the database operation
+            console.error("Error adding student:", error);
+            res.status(500).send('Error adding student');
+        } else {
+            //send a success response 
+            res.redirect('payment');
+        }
+    })
+})
+
+app.get('/payment',(req, res) => {
+
+    //render HTML page with data
+    res.render('payment');
+});
+
+app.post('/addcart', (req,res) => {
+    // extract student data from the request body
+    console.log(req.body)
+    const {product,image,price, stuffedtoys, special_request, chocolates, special_message_opt, special_message, special_packaging, special_packaging_way } = req.body;
+    const sql  = "INSERT INTO cart (image, product, price, quantity, cartorderid, customerid, stuffedtoys, special_request, chocolates, special_message_opt, special_message, special_packaging, special_packaging_way) VALUES (?, ?, ?, 1, 0, 0, ?, ?, ?, ?, ?, ?, ?)";
+    // insert the new student into the database
+    connection.query(sql, [image, product, price, stuffedtoys, special_request, chocolates, special_message_opt, special_message, special_packaging, special_packaging_way], (error, results) =>{
+        if (error) {
+            //handle any error that occurs during the database operation
+            console.error("Error adding student:", error);
+            res.status(500).send('Error adding student');
+        } else {
+            //send a success response 
+            res.redirect('/addflower');
+        }
+    })
+})
+
+app.get('/editProduct/:id', (req,res) => {
+    const cartid = req.params.id;
+    const sql = 'SELECT * FROM cart WHERE cartid = ?';
+    // fetch data from MySQL based on the student ID
+    connection.query(sql, [cartid], (error, results) => {
+        if (error){
+            console.error('Database query error :', error.message);
+            return res.status(500).send('Error Retrieving student by ID');
+        }
+        //check if any student with the gievn ID was found
+        if (results.length > 0) {
+            //render HTML page with the student data
+            res.render('editProduct', {cart: results[0]});
+        } else {
+            // if no student with the given ID was found, render a 404 page or handle it accordingly
+            res.status(404).send('Student not found');
+        }
+    });
+})
+
+app.post('/editProduct/:id', (req,res) => {
+    const cartid = req.params.id;
+    // extract student data from the request body
+    const {quantity} = req.body;
+    const sql  = "UPDATE cart SET quantity = ? WHERE cartid = ?";
+    // insert the new student into the database
+    connection.query(sql, [quantity,cartid], (error, results) =>{
+        if (error) {
+            //handle any error that occurs during the database operation
+            console.error("Error adding student:", error);
+            res.status(500).send('Error adding student');
+        } else {
+            //send a success response 
+            res.redirect('/cart');
+        }
+    })
+})
+
+app.get('/deleteProduct/:id', (req,res) => {
+    const cartid = req.params.id;
+    const sql  = "DELETE FROM cart WHERE cartid = ?";
+    // insert the new student into the database
+    connection.query(sql, [cartid], (error, reults) =>{
+        if (error) {
+            //handle any error that occurs during the database operation
+            console.error("Error adding student:", error);
+            res.status(500).send('Error adding student');
+        } else {
+            //send a success response 
+            res.redirect('/cart');
+        }
+    })
+})
 
 // // For reference
 // app.get('/student/:id', (req,res) => {
